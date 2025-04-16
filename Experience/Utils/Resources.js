4f9842d0e6bd21358068838d36cfc7e39f22b439
keno1420 <81666742+keno1420@users.noreply.github.com>
2025-04-16 18:50:15 -0400

    Update Resources.js

--- Experience/Utils/Resources.js
+++ Experience/Utils/Resources.js
@@ -30,16 +30,37 @@ export default class Resources extends EventEmitter {
         this.loaders.dracoLoader.setDecoderPath("/draco/");
         this.loaders.gltfLoader.setDRACOLoader(this.loaders.dracoLoader);
     }
-    startLoading() {
-        for (const Asset of this.Assets) {
-            if (Asset.type === "glbModel") {
-                this.loaders.gltfLoader.load(Asset.path, (file) => {
-                    this.singleAssetLoaded(Asset, file);
-                });
-            } 
-        }
-    }
-
+   startLoading() {
+         for (const Asset of this.Assets) {
+             if (Asset.type === "glbModel") {
+                 this.loaders.gltfLoader.load(Asset.path, (file) => {
+                     this.singleAssetLoaded(Asset, file);
+                 });
+             } else if (Asset.type === "videoTexture") {
+                 this.video = {};
+                 this.videoTexture = {};
+ 
+                 this.video[Asset.name] = document.createElement("video");
+                 this.video[Asset.name].src = Asset.path;
+                 this.video[Asset.name].muted = true;
+                 this.video[Asset.name].playsInline = true;
+                 this.video[Asset.name].autoplay = true;
+                 this.video[Asset.name].loop = true;
+                 this.video[Asset.name].play();
+ 
+                 this.videoTexture[Asset.name] = new THREE.VideoTexture(
+                     this.video[Asset.name]
+                 );
+                 // this.videoTexture[Asset.name].flipY = false;
+                 this.videoTexture[Asset.name].minFilter = THREE.NearestFilter;
+                 this.videoTexture[Asset.name].magFilter = THREE.NearestFilter;
+                 this.videoTexture[Asset.name].generateMipmaps = false;
+                 this.videoTexture[Asset.name].encoding = THREE.sRGBEncoding;
+ 
+                 this.singleAssetLoaded(Asset, this.videoTexture[Asset.name]);
+             }
+         }
+     }
     singleAssetLoaded(Asset, file) {
         this.items[Asset.name] = file;
         this.loaded++;
